a3d8af4fe635a1d26d6356115866bfa9c9a6ebf1
AlastorTh <alexkasatkin90@gmail.com>
2021-02-01 13:04:15 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	MakeRequest()
+}
+
+func MakeRequest() {
+	resp, err := http.Get("https://httpbin.org/get")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	resp.Body.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(string(body))
+}
